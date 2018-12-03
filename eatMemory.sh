b1771aad497d4a0950416e7c5c2cabed8e4cf348
Chetan Mehrotra <chetanm@apache.org>
2018-12-03 13:54:55 +0530

    Slightly increase memory

--- eatMemory.sh
+++ eatMemory.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 swapoff -a
-dd if=/dev/zero of=/dev/shm/fill bs=1MB count=3000
+dd if=/dev/zero of=/dev/shm/fill bs=1MB count=3500
 free -h
 docker run --name oom-check-node  --cpu-shares 128 --memory 256m --memory-swap 256m --network bridge -e __OW_API_HOST=https://172.17.0.1:443  --cap-drop NET_RAW --cap-drop NET_ADMIN --ulimit nofile=1024:1024 --pids-limit 1024 -v "$PWD":/usr/src/app -w /usr/src/app openwhisk/nodejs6action node memoryWithGC.js
