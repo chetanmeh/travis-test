0795dc2a4f6367fb901b8db351ea8148fa3904cf
Chetan Mehrotra <chetanm@apache.org>
2018-12-03 13:33:51 +0530

    Check memory

--- eatMemory.sh
+++ eatMemory.sh
@@ -8,8 +8,10 @@ function malloc() {
     if [[ $N -gt $1 ]] ;then
       N=$1
     fi
-    sh -c "MEMBLOB=\$(dd if=/dev/urandom bs=1MB count=$N) ; sleep 1"
+    sh -c "MEMBLOB=\$(dd if=/dev/zero bs=1MB count=$N) ; sleep 1"
   fi
 }
 
 malloc 6000
+free -h
+docker run --name oom-check-node  --cpu-shares 128 --memory 256m --memory-swap 256m --network bridge -e __OW_API_HOST=https://172.17.0.1:443  --cap-drop NET_RAW --cap-drop NET_ADMIN --ulimit nofile=1024:1024 --pids-limit 1024 -v "$PWD":/usr/src/app -w /usr/src/app openwhisk/nodejs6action node memoryWithGC.js
