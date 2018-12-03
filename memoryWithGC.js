0c9a9d8c76b66c2e49d956f6a5a946b0412068cf
Chetan Mehrotra <chetanm@apache.org>
2018-12-03 10:37:16 +0530

    Execute file

---
+++ memoryWithGC.js
@@ -0,0 +1,18 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+function eat(memoryMB) {
+    var bytes = 1*1024*1024*memoryMB;
+    var buffer = new Buffer.alloc(bytes, 'a');
+    buffer = null;
+    console.log('done.');
+}
+
+function main(msg) {
+    console.log('helloEatMemory', 'memory ' + msg.payload + 'MB');
+    global.gc();
+    eat(msg.payload);
+    return {msg: 'OK, buffer of size ' + msg.payload + ' MB has been filled.'};
+}
+
+main({payload:256})
